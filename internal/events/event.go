f5c28c5ef12824f1d8c135769fe2717f23f00235
Alek Dembowski <encinarus@gmail.com>
2023-05-09 09:36:54 -0400

    Lots of normalization seeding bgg

--- internal/events/event.go
+++ internal/events/event.go
@@ -191,6 +191,7 @@ func NormalizeEvent(event *GenconEvent) *GenconEvent {
 		"Anna's Roundtable":                        "Anna's Roundtable: The Fire Emblem Board Game",
 		"Ascension: Tactics":                       "Ascension Tactics: Miniatures Deckbuilding Game",
 		"Ascension; Tactics":                       "Ascension Tactics: Miniatures Deckbuilding Game",
+		"Ascension Tactics":                        "Ascension Tactics: Miniatures Deckbuilding Game",
 		"Boss Monster":                             "Boss Monster: The Dungeon Building Card Game",
 		"Captain is Dead":                          "The Captain Is Dead",
 		"Carcassone":                               "Carcassonne",
@@ -199,18 +200,107 @@ func NormalizeEvent(event *GenconEvent) *GenconEvent {
 		"Codenames Duet":                           "Codenames: Duet",
 		"Cartographers: A Roll Player Tale":        "Cartographers",
 		"Wrath of the Lich King":                   "World of Warcraft: Wrath of the Lich King",
-
+		"Axis & Allies 1942": "Axis & Allies: 1942",
+		"B-17 Queen of the Skies": "B-17: Queen of the Skies",
+		"Battle for Greyport": "The Red Dragon Inn: Battle for Greyport",
+		"Broadsides and Boarding Parties": "Broadsides & Boarding Parties",
 		"Manhattan Project: Energy Empire":          "The Manhattan Project: Energy Empire",
 		"Extraordinary Adventures: Pirates!":        "Extraordinary Adventures: Pirates",
 		"Fangs: Werewolves vs. Vampires vs. Humans": "Fangs: Werewolves vs Vampires vs Humans",
 		"Ascension": "Ascension: Deckbuilding Game",
+		"Affliction": "AFFLICTION: Salem 1692",
+		"5 Year Mission": "Star Trek: Five-Year Mission",
+		"Agatha Christie: Death in the Cards": "Agatha Christie: Death on the Cards",
+		"Age of Mythology": "Age of Mythology: The Boardgame",
+		"Alien - Fate of the Nostromo" : "ALIEN: Fate of the Nostromo",
+		"Anna's Roundtable: The Fire Emblem Board Game": "Anna's Roundtable: The Fan Made Fire Emblem Board Game",
+		"Broken and Beautiful": "Broken and Beautiful: A Game About Kintsugi",
+		"Cache Me If You Can!": "Cache Me If You Can!: The Geocaching Board Game",
+		"Cartographers: Heroes": "Cartographers Heroes",
+		"Castle Ravenloft": "Dungeons & Dragons: Castle Ravenloft Board Game",
+		"Caverna": "Caverna: The Cave Farmers",
+		"Conan by Monolith": "Conan",
+		"Conquest Princess": "Conquest Princess: Fashion Is Power",
+		"Decorum": "Décorum",
+		"Destination Neptune": "Destination: Neptune",
+		"Disney Sorcerer's Arena: Epic Alliances": "Disney Sorcerer's Arena: Epic Alliances Core Set",
+		"Downfall of Pompeii": "The Downfall of Pompeii",
+		"Dragon Prince: Battlecharged": "The Dragon Prince: Battlecharged",
+		"Dungeons & Dragons: The Yawning Portal Board Game": "Dungeons & Dragons: The Yawning Portal",
+		"E.T.I. Estimated Time to Invasion": "E.T.I.: Estimated Time to Invasion",
+ 		"Empyreal": "Empyreal: Spells & Steam",
+ 		"Escape the Dark": "The Last of Us: Escape the Dark",
+ 		"Faeries and Magical Creatures": "Faeries & Magical Creatures",
+ 		"Fateforge": "Fateforge: Chronicles of Kaan",
+ 		"Genshin Tarot": "Genshin Tarot: The Fan Made Genshin Impact Board Game",
+ 		"Great British Baking show": "The Great British Baking Show Game",
+ 		"Headless Horseman": "Headless Horseman Board Game",
+ 		"Hellboy The Board Game": "Hellboy: The Board Game",
+ 		"Hulk Smash": "The Incredible Hulk Smash",
+ 		"Ierusalem": "Ierusalem: Anno Domini",
+ 		"Kinfire Chronicles": "Kinfire Chronicles: Night's Fall",
+ 		"Kung-Fu Zoo": "Kung Fu Zoo",
+ 		"Kutna Hora": "Kutná Hora: The City of Silver",
+ 		"Ladies and Gentlmen": "Ladies & Gentlemen",
+ 		"Last Night on Earth": "Last Night on Earth: The Zombie Game",
+ 		"Legacy's Allure": "Legacy's Allure: Season 1",
+ 		"Life of the Amazoia": "Life of the Amazonia",
+ 		"Masters of Orion": "Master of Orion: The Board Game",
+ 		"My Little Pony Adventures in Equestria Deck-Building Game": "My Little Pony: Adventures in Equestria Deck-Building Game",
+ 		"Oath": "Oath: Chronicles of Empire and Exile",
+ 		"Oltree": "Oltréé",
+ 		"Orleans": "Orléans",
+ 		"Overboss": "Overboss: A Boss Monster Adventure",
+ 		"Persona 5 Royal": "Trick Gear: Persona 5 The Royal",
+ 		"Planted": "Planted: A Game of Nature & Nurture",
+ 		"Red Dragon Inn": "The Red Dragon Inn",
+ 		"Roll Camera": "Roll Camera!: The Filmmaking Board Game",
+ 		"Roll to the Top": "Roll to the Top!",
+ 		"SHOBU": "SHŌBU",
+ 		"Settlers of America": "Catan Histories: Settlers of America – Trails to Rails",
+ 		"Settlers of Catan": "The Settlers of Catan",
+ 		"Shadowgate the Living Castle": "Shadowgate: The Living Castle",
 
+ 		"Smash Up: Disney Style!": "Smash Up: Disney Edition",
+ 		"Snow White Gemstone Mining": "Snow White and the Seven Dwarfs: A Gemstone Mining Game",
+ 		"Star Trek Ascendency": "Star Trek: Ascendancy",
+ 		"Stupid Death": "Stupid Deaths",
+ 		"Sushi Go Party": "Sushi Go Party!",
+ 		"Suspects: Adele & Neville, Investigative Reporters": "Suspects: Adele and Neville, Investigative Reporters",
+ 		"The Binding Of Isaac Four Souls": "The Binding of Isaac: Four Souls",
+ 		"Trekking": "Trekking the World",
+ 		"Trogdor!!": "Trogdor!!: The Board Game",
+ 		"Tzolk'in": "Tzolk'in: The Mayan Calendar",
+ 		"Unmatched": "Unmatched Game System",
+ 		"Uproot Arboreal Battleship": "Uproot: Arboreal Battleship",
+ 		"Villainous": "Disney Villainous",
+ 		"Viticulture World": "Viticulture World: Cooperative Expansion",
+ 		"Way Too Many Cats": "Way Too Many Cats!",
+ 		"World of Ulos": "Dawn of Ulos",
+ 		"Wrath of Ashardalon": "Dungeons & Dragons: Wrath of Ashardalon Board Game",
+		"Zombie Survival": "Zombie Survival: The Board Game",
+		
 		// The difference on this one is the emdash!
 		"Disney: The Haunted Mansion - Call of the Spirits Game": "Disney: The Haunted Mansion – Call of the Spirits Game",
 	}
 
 	if canonicalSystem, found := systemRemappings[event.GameSystem]; found {
 		event.GameSystem = canonicalSystem
+	}
+
+	if event.GameSystem == "Scythe: Invaders from Afar Expansion" {
+		event.GameSystem = "Scythe"
+		event.RulesEdition = "Invaders from Afar Expansion"
+	}
+
+	if event.GameSystem == "Roll Camera! with B-Movie expansion" {
+		event.GameSystem = "Roll Camera!: The Filmmaking Board Game"
+		event.RulesEdition = "Roll Camera!: The B-Movie Expansion"
+	}
+
+	if event.GameSystem == "Betrayal at House on the Hill: Widows Walk Expansion" {
+		event.GameSystem = "Betrayal at House on the Hill"
+		event.RulesEdition = "Widows Walk Expansion"
 	}
 
 	if event.GameSystem == "Dominion" && event.RulesEdition == "Intrigue" {
