a6ec7b1b9e0057545b765dbcc520565eff1c5b60
Alek Dembowski <encinarus@gmail.com>
2026-05-10 22:40:09 -0400

    Add update log

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,13 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ApiService } from './services/api.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, DatePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('ui');
+  protected readonly lastUpdate = signal<string | null>(null);
+  private api = inject(ApiService);
+
+  ngOnInit() {
+    this.api.getLastUpdate().subscribe(data => {
+      this.lastUpdate.set(data.lastUpdate);
+    });
+  }
 }
